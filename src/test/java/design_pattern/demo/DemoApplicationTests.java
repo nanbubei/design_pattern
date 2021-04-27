221435d47f2b7c6cc1d6d66634ed0c550f33db3f
weixiangnan <13811658342@163.com>
2021-04-27 14:28:43 +0800

    first commit

--- src/test/java/design_pattern/demo/DemoApplicationTests.java
+++ src/test/java/design_pattern/demo/DemoApplicationTests.java
@@ -2,12 +2,17 @@ package design_pattern.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import sun.net.util.IPAddressUtil;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		throw new IllegalArgumentException("");
 	}
 
 }
